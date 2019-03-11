8d18b2927b24ab35f7985fb2a7092e077e0697a8
Henry Sztanski <henry@sztanski.com>
2019-03-11 12:07:30 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,36 @@
+class Song
+  attr_accessor :name, :artist, :genre
+
+  @@count = 0
+  @@genres = []
+  @@artists = []
+
+  def initialize(name, artist, genre)
+    @@count += 1
+    @name = name
+    @artist = artist
+    @@artists << artist
+    @genre = genre
+    @@genres << genre
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genre_count
+    hashy = Hash.new
+    @@genres.each do |genre|
+      hashy.key?(genre) ? hashy[genre] += 1 : hashy[genre] = 1
+    end
+    hashy
+  end
+end

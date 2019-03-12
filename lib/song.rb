e12c7d72612cc935ae866c2571182891ac6fd851
Eyal Toledano <eyaltoledano@me.com>
2019-03-12 02:38:06 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,46 @@
+require 'pry'
+
+class Song
+  attr_accessor :name, :artist, :genre
+
+  @@count = 0
+  @@artists = []
+  @@genres = []
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@artists << artist
+    @@genres << genre
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.genre_count
+    counts = Hash.new(0)
+    binding.pry
+    @@genres.each do |genre|
+
+      unless @@genres.include?(genre)
+
+      end
+    end
+    counts
+  end
+
+  def self.artist_count
+  end
+
+end

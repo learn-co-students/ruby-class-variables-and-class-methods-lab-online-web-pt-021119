f93aa31b91eb5b5e913fcd5bcc718dd29d5e2eb7
Nash Brancic <nashfft@gmail.com>
2019-03-21 03:40:08 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,16 +1,15 @@
 class Song
   attr_accessor :name, :artist, :genre
   
-  #@@count = 0
   @@artists = []
   @@genres = []
-#@@genre_count = {}
+  @@count = 0
   
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
-    @@count = 3
+    @@count += 1
     @@artists << artist
     @@genres << genre
   end
